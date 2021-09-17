0036c91f02076f55b20b09280080cc5ef82ba05a
Alberto Costa <acosta@itisrossi.vi.it>
2021-09-17 09:41:49 +0200

    solution

---
+++ CountingTheNumberOfCharacters/CountingTheNumberOfCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CountingTheNumberOfCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n**************** START ****************\n\n");
+
+            //
+
+            string word = "";
+
+            while (word.Trim() == "") {
+
+                Console.Write("What is the input string ?\t");
+
+                word = Console.ReadLine();
+            }
+
+            word = word.Replace(" ", "");
+
+            Console.WriteLine(word);
+
+            int wordLength = word.Length;
+
+            Console.WriteLine($"{word} has {wordLength} characters");
+        }
+    }
+}
