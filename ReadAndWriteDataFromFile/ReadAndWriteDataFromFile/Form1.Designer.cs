bbe1db2e4e043f54d5fc7c1519caec12b75ebe14
Alberto Costa <acosta@itisrossi.vi.it>
2021-10-23 15:40:01 +0200

    Read and Write data from file with ListBox selection

---
+++ ReadAndWriteDataFromFile/ReadAndWriteDataFromFile/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ReadAndWriteDataFromFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxPeople = new System.Windows.Forms.ListBox();
+            this.listBoxSelected = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonWriteIntoFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxPeople
+            // 
+            this.listBoxPeople.FormattingEnabled = true;
+            this.listBoxPeople.Location = new System.Drawing.Point(22, 103);
+            this.listBoxPeople.Name = "listBoxPeople";
+            this.listBoxPeople.Size = new System.Drawing.Size(315, 381);
+            this.listBoxPeople.TabIndex = 1;
+            this.listBoxPeople.SelectedIndexChanged += new System.EventHandler(this.listBoxPeople_SelectedIndexChanged);
+            // 
+            // listBoxSelected
+            // 
+            this.listBoxSelected.FormattingEnabled = true;
+            this.listBoxSelected.Location = new System.Drawing.Point(354, 103);
+            this.listBoxSelected.Name = "listBoxSelected";
+            this.listBoxSelected.Size = new System.Drawing.Size(318, 381);
+            this.listBoxSelected.TabIndex = 2;
+            this.listBoxSelected.SelectedIndexChanged += new System.EventHandler(this.listBoxSelected_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Read data from file and load records into ListBox.\r\nMove data beetween 2 listbox\r" +
+    "\n";
+            // 
+            // buttonWriteIntoFile
+            // 
+            this.buttonWriteIntoFile.Location = new System.Drawing.Point(217, 517);
+            this.buttonWriteIntoFile.Name = "buttonWriteIntoFile";
+            this.buttonWriteIntoFile.Size = new System.Drawing.Size(256, 23);
+            this.buttonWriteIntoFile.TabIndex = 4;
+            this.buttonWriteIntoFile.Text = "Write selected into file";
+            this.buttonWriteIntoFile.UseVisualStyleBackColor = true;
+            this.buttonWriteIntoFile.Click += new System.EventHandler(this.buttonWriteIntoFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 638);
+            this.Controls.Add(this.buttonWriteIntoFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxSelected);
+            this.Controls.Add(this.listBoxPeople);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxPeople;
+        private System.Windows.Forms.ListBox listBoxSelected;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonWriteIntoFile;
+    }
+}
+
---
+++ ReadAndWriteDataFromFile/ReadAndWriteDataFromFile/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadAndWriteDataFromFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+        void Init()
+        {
+            string[] dataFile = File.ReadAllLines(@"..\..\Data.txt");
+
+            string[,] people = new string[dataFile.Length, 3];
+
+            int y = 0;
+
+            foreach (var line in dataFile)
+            {
+                string[] data = line.Split(';');
+
+                people[y, 0] = data[0];
+                people[y, 1] = data[1];
+                people[y, 2] = data[2];
+
+                y++;
+            }
+
+            for (int i = 0; i < people.GetLength(0); i++)
+            {
+                listBoxPeople.Items.Add($"{ people[i, 0] },{ people[i, 1] },{ people[i, 2] }");
+            }
+        }
+
+        private void listBoxPeople_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var itemSelected = listBoxPeople.SelectedItem;
+
+            if (itemSelected == null) return;
+
+            listBoxSelected.Items.Add(itemSelected);
+
+            listBoxPeople.Items.Remove(listBoxPeople.SelectedItem);
+        }
+
+        private void listBoxSelected_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var itemSelected = listBoxSelected.SelectedItem;
+
+            if (itemSelected == null) return;
+
+            listBoxPeople.Items.Add(itemSelected);
+
+            listBoxSelected.Items.Remove(listBoxSelected.SelectedItem);
+        }
+
+        private void buttonWriteIntoFile_Click(object sender, EventArgs e)
+        {   
+            string[] lines = new string[listBoxPeople.Items.Count];
+
+            int i = 0;
+
+            foreach (string item in listBoxSelected.Items)
+            {
+                lines[i] = item.Replace(", ", ";");
+
+                i++;
+            }
+
+            File.WriteAllLines(@"..\..\Result.txt", lines);
+        }
+    }
+}
