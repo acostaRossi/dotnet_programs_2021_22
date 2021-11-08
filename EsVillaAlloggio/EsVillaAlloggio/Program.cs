63261aa1b8cbd4f70156a23ea50eeda65b835d2a
Alberto Costa <73739928+acostaRossi@users.noreply.github.com>
2021-11-08 15:50:09 +0100

    Esercizio Ereditarieta Libro

---
+++ EsVillaAlloggio/EsVillaAlloggio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EsVillaAlloggio
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Villa villa = new Villa("Top", 4, 200, 1000);
+
+            Console.WriteLine(villa.CostoAcqua(10));
+        }
+    }
+}
